ff4d9b120a3aa46ecf143df47ed3b3206525f9e3
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-12-08 22:44:33 +0300

    set up more Input component tests

--- 03_jotto/src/Input.test.js
+++ 03_jotto/src/Input.test.js
@@ -1,18 +1,22 @@
 import { shallow } from 'enzyme';
 
-import { findByTestAttr } from '../test/testUtils';
+import { findByTestAttr, checkProps } from '../test/testUtils';
 import Input from './Input';
 
 /**
  * Setup function for app component
  * @returns {ShallowWrapper}
  */
-const setup = () => {
-  return shallow(<Input />);
+const setup = (secretWord = 'party') => {
+  return shallow(<Input secretWord={secretWord} />);
 };
 
 test('renders without error', () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, 'component-input');
   expect(component.length).toBe(1);
+});
+
+test('does not throw warning with expected props', () => {
+  checkProps(Input, { secretWord: 'party' });
 });
