dfdecff9c00fb59360449f5c8a60eb465dceafa1
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-12-10 18:21:56 +0300

    set up Mocking of the getSecretWord

--- 03_jotto/src/App.test.js
+++ 03_jotto/src/App.test.js
@@ -1,17 +1,43 @@
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { findByTestAttr } from '../test/testUtils';
 import App from './App';
+
+// activate global mock to make sure getSecretWord doesn't make network call
+jest.mock('./actions');
+// eslint-disable-next-line
+import { getSecretWord as mockGetSecretWord } from './actions';
 
 /**
  * Setup function for App component
  * @returns {ShallowWrapper}
  */
 const setup = () => {
-  return shallow(<App />);
+  return mount(<App />);
 };
 
 test('renders without errors', () => {
   const wrapper = setup();
   const appComponent = findByTestAttr(wrapper, 'component-app');
   expect(appComponent).toHaveLength(1);
+});
+
+describe('get secret word', () => {
+  beforeEach(() => {
+    // clear the mock calls from previous tests
+    mockGetSecretWord.mockClear();
+  });
+
+  test('getSecretWord on app mount', () => {
+    const wrapper = setup();
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(1);
+  });
+
+  test('getSecretWord does not run on app update', () => {
+    const wrapper = setup();
+    mockGetSecretWord.mockClear();
+
+    wrapper.setProps();
+
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(0);
+  });
 });
