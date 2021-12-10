f99e10422776aaa9236bcd61c7aaa0f59c662f89
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-12-10 16:13:39 +0300

    basic setup of axios and moxios

---
+++ 03_jotto/src/actions/index.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+export const getSecretWord = () => {
+  //TODO: write actual action in Redux / Context section
+  return axios.get('http://localhost:3030').then((response) => response.data);
+};
