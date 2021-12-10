62a66526db0fa6d48b1b0dba2019ee29429908ca
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-12-10 19:06:51 +0300

    init project

---
+++ 04_jotto-with-redux/src/Input.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Input = ({ success, secretWord }) => {
+  const [currentGuess, setCurrentGuess] = React.useState('');
+
+  if (success) {
+    return <div data-test="component-input" />;
+  }
+
+  return (
+    <div data-test="component-input">
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          type="text"
+          placeholder="Enter guess"
+          value={currentGuess}
+          onChange={(event) => setCurrentGuess(event.target.value)}
+        />
+        <button
+          data-test="submit-button"
+          className="btn btn-primary mb-2"
+          onClick={(evt) => {
+            evt.preventDefault();
+            // TODO: update guessedWords
+            // TODO: check against secretWord and update success if needed
+            setCurrentGuess('');
+          }}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired,
+};
+
+export default Input;
