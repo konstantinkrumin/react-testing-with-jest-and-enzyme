1c9082fc79d73089aaf652770c4c7176bb2d59c8
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-12-10 19:14:54 +0300

    set up redux

---
+++ 04_jotto-with-redux/src/reducers/successReducer.js
@@ -0,0 +1,11 @@
+/**
+ * @function successReducer
+ * @param {boolean} state - previous state.
+ * @param {object} action - action to be reduced.
+ * @returns {boolean} - new success state.
+ */
+const successReducer = (state, action) => {
+  return null;
+};
+
+export default successReducer;
